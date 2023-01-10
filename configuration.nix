0151052f0a75ec9cb99a4b42b47c77a7d7e21a26
Floki89 <tw@wallpen.com>
2023-01-10 09:41:50 +0100

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -1,3 +1,7 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
 { config, pkgs, ... }:
 
 {
@@ -5,146 +9,58 @@
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
     ];
+    
+boot.loader.systemd-boot.enable = true;
 
-  # Use the systemd-boot EFI boot loader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+boot.initrd.luks.devices = [
+  {
+    name = "root";
+    devices = "/dev/nvme0n1p2";
+    preLVM = true;
+  }
+];
 
- boot.kernelPackages = pkgs.linuxPackages_latest;
+# Select internationalisation properties
+i18n = (
+  consoleKeyMap = "de";
+  defaultLocale = "en_US.UTF-8"
+);
 
-  # Set your time zone.
-  time.timeZone = "Europe/Berlin";
+# Set your time zone
+time.timeZone = "Europe/Berlin";
 
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
-  console = {
-    font = "Lat2-Terminus16";
-    keyMap = "de";
-  };
+networking.networkmanager.enable = true;
 
-  # Supposedly better for the SSD.
-  fileSystems."/".options = [ "noatime" "nodiratime" "discard" ];
+# List packages installed in system profile. To search, run:
+# $ nix search wget
+environment.systemPackages = with pkgs: [
+  networkmanagerapplet
+  vim
+  wget
+  docker-compose
+  htop
+  feh
+];
 
-  # Configure keymap in X11
-  services.xserver.enable = true;
-  services.xserver.videoDrivers = [ "amdgpu" ];
-  services.xserver.windowManager.ratpoison.enable = true;
+# Backlight control
+programs.light.enable = true;
+  
+# Enable the X11 windowing system.
+services.xserver.enable = true;
+services.xserver.layout = "de";
 
-  services.xserver.layout = "de";
-  services.xserver.xkbVariant = "neo";
-  services.xserver.xkbOptions = "ctrl:swap_lalt_lctl_lwin";
+services.xserver.windowManager.i3.enable = true;
+services.xserver.autorun = true;
 
-  # Enable sound.
-  sound.enable = true;
-  security.rtkit.enable = true;
-  hardware.pulseaudio.enable = true;
+services.xserver.libinput.enable = true;
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  services.xserver.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.mutableUsers = false;
-  users.users.tobi = {
-    isNormalUser = true;
-    shell = pkgs.fish;
-    home = "/home/tobi";
-    hashedPassword = "tobi";
-    extraGroups = [ "wheel" "docker" "audio" "video" ];
-  };
-
-  # nix options for derivations to persist garbage collection
-  nix.extraOptions = ''
-    keep-outputs = true
-    keep-derivations = true
-  '';
-  environment.pathsToLink = [
-    "/share/nix-direnv"
-  ];
-
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  nixpkgs.config.allowUnfree = true;
-  nixpkgs.overlays = [
-    (import (builtins.fetchTarball https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz))
-  ];
-    python3withPackages = pkgs.python3.withPackages(ps: with ps; [
-      pandas numpy matplotlib scikitlearn notebook chess opencv4 z3
-    ]);
-  in
-  [
-    direnv nix-direnv
-    wget
-    vim emacs
-    ispell
-    git
-    mpv cmus spotify
-    alacritty
-    mkpasswd
-    rdiff-backup
-    pass
-    dmenu
-    arandr xclip
-    tdesktop
-    thunderbird
-    trash-cli
-    acpi
-    usbutils # for lsusb
-    gimp inkscape
-    imagemagick graphviz
-    ghc stack hlint
-    latest.rustChannels.stable.rust
-    rust-analyzer
-    mpd mpc_cli
-    docker-compose
-    python3withPackages
-    nodejs
-    clang
-    htop
-    feh
-    unzip
-    mupdf
-    gnumake
-    ncurses portmidi
-    wmname
-    # arduino
-    youtube-dl
-    element-desktop
-    tarsnap
-    audacity
-    gnuplot
-    stockfish
-    libnotify
-    gnugo gogui
-    asymptote
-    coq
-  ];
-
-  fonts.fonts = with pkgs; [
-    fira-code
-    fira-code-symbols
-  ];
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  programs.gnupg.agent = {
-    enable = true;
-  };
-
-  # Backlight control
-  programs.light.enable = true;
-
-  location = {
-    latitude = 50.87;
-    longitude = 8.02;
-  };
-  services.redshift.enable = true;
-
-  programs.fish.enable = true;
-
-  virtualisation.docker.enable = true;
-
-  };
-
-  system.stateVersion = "20.09"; # Did you read the comment?
-}
+# Define a user account. Don't forget to set a password with with passwd
+user.extraUsers.tobi = {
+  createHome = true;
+  extraGroups = ["wheel" "video" "audio" "disk" "networkmanager"];
+  group = "users";
+  isNormalUser = true;
+  shell = pkgs.fish;
+  home = "/home/tobi";
+  uid = 1000;
+};
